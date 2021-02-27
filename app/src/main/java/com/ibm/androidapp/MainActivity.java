41aa86d14ec2187d650b7b67f222902974047f8d
Alba Vicente Olmo <albavicenteolmo@MacBook-Pro-de-Alba.local>
2021-02-27 13:14:20 +0100

    Cambios Alba hasta las 13

--- app/src/main/java/com/ibm/androidapp/MainActivity.java
+++ app/src/main/java/com/ibm/androidapp/MainActivity.java
@@ -1,11 +1,19 @@
 package com.ibm.androidapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +24,31 @@ public class MainActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
         //setupWebView((WebView) findViewById(R.id.webView));
+
+        ImageView menu_icon = findViewById(R.id.menu_icon);
+        registerForContextMenu(menu_icon);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.menu, menu)
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+         switch(item.getItemId()){
+             case R.id.action_alimentacion:
+                 Toast.makeText(this, "Alimentación", Toast.LENGTH_SHORT).show();
+                 return true;
+         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
 
     protected void setupWebView(WebView webView) {
